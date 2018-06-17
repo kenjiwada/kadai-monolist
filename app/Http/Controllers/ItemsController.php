562e79617659954c9a6a84d6d1a5810c4e97770f
EC2 Default User <ec2-user@ip-172-31-38-111.us-east-2.compute.internal>
2018-06-17 12:40:58 +0000

    check

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -1,4 +1,7 @@
-use \App\Item;
+<?php
+
+  use \App\Item;
+  namespace App\Http\Controllers;
 
   class ItemsController extends Controller
   {
@@ -38,10 +41,12 @@ use \App\Item;
     {
       $item = Item::find($id);
       $want_users = $item->want_users;
+      $have_users = $item->have_users;
 
       return view('items.show', [
           'item' => $item,
           'want_users' => $want_users,
+          'have_users' => $have_users,
       ]);
     }
   }
